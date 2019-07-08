c22c470801063bac297dbbf4c862464281c606c0
Guido Santillan <guidosantillan01@gmail.com>
2019-07-08 16:26:15 -0500

    Docker Compose Commands

--- visits/index.js
+++ visits/index.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 
 const app = express();
 const client = redis.createClient({
-  host: 'redis-server',
+  host: 'redis-server', // Use the same docker-compose.yml service name here
   port: 6379
 });
 client.set('visits', 0);
